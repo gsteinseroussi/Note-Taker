61c91d3f84d77f9e1512e922a5a8998deda59fa9
Garrett Stein-Seroussi <gsteinseroussi@gmail.com>
2020-08-01 18:07:18 -0400

    adjusted the get route

--- Develop/db/notes.js
+++ Develop/db/notes.js
@@ -7,18 +7,31 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 class Note {
   readNote() {
-    const temp = readFileAsync("/db.json", "utf8");
-    console.log(temp);
-    return temp;
+    console.log("began reading");
+    const allNotes = readFileAsync("./db/db.json", "utf8", function (
+      err,
+      data
+    ) {
+      if (err) {
+        throw err;
+      }
+      return JSON.stringify(data);
+    }).then(function (data) {
+      return data;
+    });
     //  return readFileAsync("db/db.json", "utf8");
+    console.log(allNotes);
+    return allNotes;
   }
   writeNote(note) {
-    return writeFileAsync("/db.json", JSON.stringify(note));
+    return writeFileAsync("./db/db.json", JSON.stringify(note));
   }
   getNotes() {
+    console.log("running get notes");
     return this.readNote().then(function (notes) {
+      console.log(notes + "console log notes");
       let notesArray = [];
-      console.log(notes);
+
       try {
         notesArray = [].concat(JSON.parse(notes));
         console.log(notes + "dasfa");
